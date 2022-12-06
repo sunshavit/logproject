f7016188ff2876ffecf705fb92c638def33173be
Yonatan Snir <yonatans@radware.com>
2022-12-06 16:21:29 +0200

    add case page

--- src/components/CaseTable/CaseTable.jsx
+++ src/components/CaseTable/CaseTable.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { TableWrapper } from "./CaseTable.style";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
+import { AppContext } from "../../App";
 
 const columns = [
   { field: "id", headerName: "ID", width: 90, sortable: false },
@@ -29,38 +30,15 @@ const columns = [
   },
 ];
 
-const rows = [
-  {
-    id: 1,
-    caseName: "SC1115",
-    date: 1652913512000,
-    customer: "Bank Of Colombia",
-  },
-  { id: 2, caseName: "SC1455", date: 1670333912000, customer: "Tiger Lili" },
-  { id: 3, caseName: "SC7785", date: 1670506712000, customer: "Mitzi" },
-  { id: 4, caseName: "SC9652", date: 1655566712000, customer: "Amazon" },
-  { id: 5, caseName: "SC8456", date: 1660837112000, customer: "Nit LTD" },
-  { id: 6, caseName: "SC9652", date: 1645223912000, customer: "Sun RobotiSC" },
-  { id: 7, caseName: "SC5565", date: 1652913512000, customer: "Adon Menashe" },
-  {
-    id: 8,
-    caseName: "SC2223",
-    date: 1652913512000,
-    customer: "SuperX Software",
-  },
-  {
-    id: 9,
-    caseName: "SC5696",
-    date: 1652913512000,
-    customer: "Yoni Boston Hadati",
-  },
-];
-
 const CaseTable = () => {
+  const { casesRows } = useContext(AppContext);
+  const navigate = useNavigate();
+
   const handleEvent = (params) => {
-    console.log("/" + params.id, redirect);
-    <Navigate to={`/${params.id}`} />;
+    navigate(`/${params.id}`);
   };
+
+  if (casesRows.isLoading) return <h3>Loading...</h3>
 
   return (
     <TableWrapper>
@@ -75,7 +53,7 @@ const CaseTable = () => {
         }}
       >
         <DataGrid
-          rows={rows}
+          rows={casesRows.data}
           columns={columns}
           pageSize={9}
           rowsPerPageOptions={[9]}
--- src/pages/CasePage/CasePage.jsx
+++ src/pages/CasePage/CasePage.jsx
@@ -1,3 +1,16 @@
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { AppContext } from '../../App';
+
+
 export function CasePage() {
-    return <div>Hello World</div>
+    const { casesRows } = useContext(AppContext);
+    const { caseId } = useParams();
+
+    if (!casesRows.data) return <h3>Loading...</h3>
+    const row = casesRows.data.find(row => row.id === parseInt(caseId));
+    console.log(row);
+    return <div>
+        {JSON.stringify(row)}
+    </div>
 }
