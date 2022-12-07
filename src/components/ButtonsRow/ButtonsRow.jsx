1991691cfdb7357f438fb45a8fc5a62c4ca765f3
Yodana <yodana@radware.com>
2022-12-07 10:35:41 +0200

    time picker

--- src/components/ButtonsRow/ButtonsRow.jsx
+++ src/components/ButtonsRow/ButtonsRow.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Button } from "../inputs/button.style";
 import TimePicker from "../TimePicker/TimePicker";
-import { ButtonsRowWrap } from "./ButtonsRow.style";
+import { ButtonsRowWrap, Container } from "./ButtonsRow.style";
 
 const ButtonsRow = () => {
   const setTimFrame = () => {};
   return (
-    <ButtonsRowWrap>
-      <Button> Auto check Profile</Button>
-      <Button> Graphana</Button>
-      <Button> Support File Logs</Button>
-      <Button> Index Modal</Button>
+    <Container>
+      <ButtonsRowWrap>
+        <Button> Auto check Profile</Button>
+        <Button> Graphana</Button>
+        <Button> Support File Logs</Button>
+        <Button> Index Modal</Button>
+      </ButtonsRowWrap>
       <TimePicker getTimeFrame={setTimFrame} />
-    </ButtonsRowWrap>
+    </Container>
   );
 };
 
--- src/components/ButtonsRow/ButtonsRow.style.js
+++ src/components/ButtonsRow/ButtonsRow.style.js
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 export const ButtonsRowWrap = styled.div`
   display: flex;
   width: 100%;
   gap: 30px;
   padding-top: 20px;
+  width: fit-content;
 `;
